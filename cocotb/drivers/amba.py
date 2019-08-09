4527b4a7270f9590658d45c2c6609b3f890f3de4
Nicola Corna <nicola@corna.info>
2019-08-09 10:56:03 +0200

    Remove ReadOnly() and NextTimeStep() calls

--- cocotb/drivers/amba.py
+++ cocotb/drivers/amba.py
@@ -33,8 +33,8 @@ import cocotb
 from cocotb.binary import BinaryValue
 from cocotb.drivers import BusDriver
 from cocotb.result import ReturnValue
-from cocotb.triggers import (ClockCycles, FallingEdge, Lock, NextTimeStep,
-                             ReadOnly, RisingEdge)
+from cocotb.triggers import (ClockCycles, FallingEdge, Lock, ReadOnly,
+                             RisingEdge)
 
 
 class AXIProtocolError(Exception):
@@ -440,9 +440,7 @@ class Axi4StreamMaster(BusDriver):
             yield RisingEdge(self.clock)
 
             while True:
-                yield ReadOnly()
                 if not hasattr(self.bus, "TREADY") or self.bus.TREADY.value:
-                    yield NextTimeStep()  # Can't write during read-only phase
                     break
                 yield RisingEdge(self.clock)
 
